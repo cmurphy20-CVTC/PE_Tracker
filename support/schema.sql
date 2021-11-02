9eda4eee67ceafaf1bc0f70ada2893a21f5346dc
Casey Murphy <cmurphy20@student.cvtc.edu>
2021-11-01 19:55:54 -0500

    adding index.ejs

--- support/schema.sql
+++ support/schema.sql
@@ -30,8 +30,3 @@ CREATE TABLE tasks (
     name TEXT NOT NULL
 );
 
-SELECT c.relname AS table, a.attname AS column, t.typname AS type FROM pg_catalog.pg_class AS c
-            LEFT JOIN pg_catalog.pg_attribute AS a ON c.oid = a.attrelid AND a.attnum > 0 LEFT JOIN pg_catalog.pg_type AS t 
-            ON a.atttypid = t.oid
-            WHERE c.relname IN('users', 'observations', 'students', 'schools', 'tasks', )
-            ORDER BY c.relname, a.attnum;
