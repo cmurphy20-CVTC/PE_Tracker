1b8ea2e94a0fed9c354d8d2ffbb6b0df634d3d26
Casey Murphy <cmurphy20@student.cvtc.edu>
2021-11-01 19:15:26 -0500

    Add /db-info route with table display

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require ('express');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
 const {Pool} = require('pg');
-
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnathorized: false
     }
 });
-
+const PORT = process.env.PORT || 5000;
 express() 
 .use(express.static(path.join(__dirname, 'public')))
 .use(express.json())
